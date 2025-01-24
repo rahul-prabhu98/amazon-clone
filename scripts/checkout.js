a3fb227060cfb6b8cf26c5fbbf15a9465ba0968e
Rahul Prabhu <rahulprabhu198@gmail.com>
2025-01-24 11:33:31 -0500

    Updated the HTML page after deleting a product from cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
   
 
     cartSummaryHtml += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -102,6 +102,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     link.addEventListener('click', () =>{
         const productId = link.dataset.productId;
         removeFromCart(productId);
-        console.log(cart);
+        
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        console.log(container);
+        container.remove();
     });
 });
